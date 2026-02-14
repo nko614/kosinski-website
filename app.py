0d2014429732afb3080a2989b745d9ce27b1090c
Nicholas Kosinski <nko@odoo.com>
2026-02-14 16:48:56 -0500

    update website with odoo apps

--- app.py
+++ app.py
@@ -249,7 +249,8 @@ ODOO_APPS = [
     {
         'name': 'Google Maps Route Optimization',
         'desc': 'Odoo module that optimizes delivery routes using the Google Maps API. Geocodes partner addresses and calculates optimal stop sequences for stock pickings.',
-        'url': 'https://github.com/nko614/odoo_customizations/tree/main/delivery_optimizer',
+        'price': '$499',
+        'icon': '<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width="40" height="40"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z" fill="#4285F4"/></svg>',
     },
 ]
 
